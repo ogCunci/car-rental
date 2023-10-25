619b0202433a4f99aec31d3036657ec6bbd6d4b7
ogCunci <bolykydani@gmail.com>
2023-10-25 14:54:21 +0200

    Added Navbar

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,11 +6,12 @@ const CustomButton = ({
   title,
   containerStyles,
   handleClick,
+  btnType,
 }: CustomButtomProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
