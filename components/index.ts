f7766123c12749d3074acc74444f687f3af324cd
ogCunci <bolykydani@gmail.com>
2023-10-25 14:30:13 +0200

    Created Hero Section

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import HeroSection from "./HeroSection";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export { HeroSection, CustomButton, Navbar, Footer };
